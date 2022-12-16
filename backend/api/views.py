cb705e2e60a81f96c4b4c1ecc645363cb75757d3
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2022-12-16 07:35:12 +0200

    Lint: views file

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,13 +7,7 @@ def api_home(request, *args, **kwargs):
   request_body = request.body
   params = request.GET
   body = None
-  try:
-    body = json.loads(request_body)
-  except:
-    pass
-  data = {
-    "body": body,
-    "params": params
-  }
-
+  try: body = json.loads(request_body)
+  except: pass
+  data = {"body": body, "params": params}
   return JsonResponse(data)
